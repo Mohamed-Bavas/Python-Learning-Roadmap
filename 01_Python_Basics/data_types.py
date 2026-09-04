633d472347a37d6d62b4715416d5c5d04dbbd194
Mohamed bavas N <mohamedbavasmohamedbavas@gmail.com>
2026-09-04 20:22:57 +0530

    added

---
+++ 01_Python_Basics/data_types.py
@@ -0,0 +1,9 @@
+name = "Mohamed"       
+age = 20               
+height = 165.5         
+is_student = True     
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
