b53712009b694e690494a6ddbaa04a557be8e47d
Mohamed bavas N <mohamedbavasmohamedbavas@gmail.com>
2026-09-18 22:33:39 +0530

    added

---
+++ 06_Data_Structures/dictionary.py
@@ -0,0 +1,8 @@
+student = {
+    "name": "Mohamed",
+    "age": 20,
+    "department": "ECE"
+}
+print("Name:", student["name"])
+print("Age:", student["age"])
+print("Department:", student["department"])
